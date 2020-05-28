e570d85c321c0ed15a1ee3e006ab2d1bd42aea74
NicoG94 <grosskreuznico@gmail.com>
2020-05-28 14:35:46 +0200

    no message

--- Code/Movie_Recommender/scripts2/S12Docker/get_requirements_file.py
+++ Code/Movie_Recommender/scripts2/S12Docker/get_requirements_file.py
@@ -1,8 +1,10 @@
 import os
 import subprocess
+import shutil
 
 # get correct path
-project_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+file_path = os.path.realpath(__file__)
+project_path = os.path.dirname(os.path.dirname(file_path))
 
 # get requirements.txt
 sys_call = f'pipreqs "{project_path}" --force'
@@ -10,4 +12,9 @@ print(sys_call)
 subprocess.call(sys_call, shell=True)
 
 # move requirements.txt
+old_path = project_path+r"\\requirements.txt"
+new_path = os.path.dirname(os.path.dirname(project_path))+r"\\requirements.txt"
+shutil.move(old_path, new_path)
+shutil.copyfile(new_path, old_path)
+
 print("new requirements.txt created")
