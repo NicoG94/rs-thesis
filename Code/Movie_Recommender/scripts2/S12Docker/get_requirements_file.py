15ce4768a282f8ad95620a210ee7e7ef26bc2d12
NicoG94 <grosskreuznico@gmail.com>
2020-05-28 16:15:56 +0200

    docker automated build working state

--- Code/Movie_Recommender/scripts2/S12Docker/get_requirements_file.py
+++ Code/Movie_Recommender/scripts2/S12Docker/get_requirements_file.py
@@ -11,10 +11,9 @@ sys_call = f'pipreqs "{project_path}" --force'
 print(sys_call)
 subprocess.call(sys_call, shell=True)
 
-# move requirements.txt
+# move requirements.txt to S12Docker
 old_path = project_path+r"\\requirements.txt"
-new_path = os.path.dirname(os.path.dirname(project_path))+r"\\requirements.txt"
+new_path = os.path.dirname(file_path)+r"\\requirements.txt"
 shutil.move(old_path, new_path)
-shutil.copyfile(new_path, old_path)
 
 print("new requirements.txt created")
