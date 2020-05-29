53f6f443c4881496bcc3e431fb477abc393e877b
NicoG94 <grosskreuznico@gmail.com>
2020-05-29 21:05:41 +0200

    no message

--- Code/Movie_Recommender/scripts2/D01get_data/get_data.py
+++ Code/Movie_Recommender/scripts2/D01get_data/get_data.py
@@ -1,3 +1,13 @@
 import pandas as pd
 
 a = pd.DataFrame({"A":[1,2]})
+print(a)
+print("print success")
+a.to_csv("csv_test_success.csv")
+
+f = open("requirements.txt")
+print(f.read())
+
+import numpy as np
+b = np.array([0,1])
+print(b)
