8334e4cf83f6fb661bebceda85c7b3f3b92191ca
NicoG94 <grosskreuznico@gmail.com>
2020-05-30 13:24:37 +0200

    no message

--- Code/Movie_Recommender/scripts2/D01get_data/get_data.py
+++ Code/Movie_Recommender/scripts2/D01get_data/get_data.py
@@ -1,29 +1,74 @@
 import pandas as pd
-import json
-import requests
 import os
+from urllib.request import urlopen
+from zipfile import ZipFile
 
-#def get_ratings():
+def download_and_save_data(url, datafolder):
+    zipurl = url
+    # Download the file from the URL
+    zipresp = urlopen(zipurl)
+    # Create a new file on the hard drive
+    tempzip = open("data_folder/tempfile.zip", "wb")
+    # Write the contents of the downloaded file into the new file
+    tempzip.write(zipresp.read())
+    # Close the newly-created file
+    tempzip.close()
+    # Re-open the newly-created file with ZipFile()
+    zf = ZipFile("data_folder/tempfile.zip")
+    # Extract its contents into <extraction_path>
+    # note that extractall will automatically create the path
+    zf.extractall(path='data_folder')
+    # close the ZipFile instance
+    zf.close()
 
-
-#def get_links():
-
-
-def get_data2():
-    a = pd.DataFrame({"A":[1,2]})
-
+def make_dir(temp_folder):
     # make folder and path
-    temp_folder = 'data'
     if not os.path.exists(temp_folder):
         os.mkdir(temp_folder)
+
+def merge_data(datafolder):
+    links_file_name = f"{datafolder}/links.csv"
+    ratings_file_name = f"{datafolder}/ratings.csv"
+
+    links = pd.read_csv(links_file_name)
+    ratings = pd.read_csv(ratings_file_name)
+
+    # merge data
+    ratingsImbd = ratings.merge(links, left_index=True, right_index=True)
+
+    file_path = os.path.join(
+        temp_folder, 'data.csv')
+
+    # upload data
+    ratingsImbd.to_csv(file_path, index=False)
+
+    # save data
+    ratingsImbd.to_csv(file_path, index=False)
+    # save path
+    with open("blob_path.txt", "w") as output_file:
+        output_file.write(file_path)
+
+def get_data2(temp_folder):
+    a = pd.DataFrame({"A":[1,2]})
+
+
     file_path = os.path.join(
         temp_folder, 'data.csv')
     # save data
     a.to_csv(file_path, index=False)
     # save path
-    with open("blob_path.txt", "w") as output_file:
+    with open("/blob_path.txt", "w") as output_file:
         output_file.write(file_path)
 
 
 if __name__ == "__main__":
-    get_data2()
+    # url = "http://files.grouplens.org/datasets/movielens/ml-latest.zip" # big
+    #url = "http://files.grouplens.org/datasets/movielens/ml-latest-small.zip"
+    temp_folder = '/data_folder'
+    make_dir(temp_folder)
+    #download_and_save_data(url, temp_folder)
+    #merge_data(temp_folder)
+
+    get_data2(temp_folder)
+
+
--- Code/Movie_Recommender/scripts2/D11kubeflow_pipeline/train_model_pipeline.py
+++ Code/Movie_Recommender/scripts2/D11kubeflow_pipeline/train_model_pipeline.py
@@ -1,5 +1,7 @@
 #importing KFP pipeline
 from kfp.dsl import pipeline
+import kfp.dsl as dsl
+from kfp.compiler import Compiler
 
 # defining pipeline meta
 @pipeline(
@@ -8,27 +10,24 @@ from kfp.dsl import pipeline
 )
 # stitch the steps
 def train_recommender_model():
-    # importing container operation
-    import kfp.dsl as dsl
-
     get_data_op = dsl.ContainerOp(
         name = 'get_data', # name of the operation
         image = 'rsthesis/get_data_image:latest', #docker location in registry
         #arguments = [context], # passing context as argument
         file_outputs = {
-            'context': '/blob_path.txt' #name of the file with result
+            'blob-path': '/blob_path.txt' #name of the file with result
         }
     )
     prepare_data_op = dsl.ContainerOp(
         name = 'prepare_data', # name of operation
         image = 'rsthesis/prepare_data_image:latest', #docker location in registry
-        arguments = [get_data_op], #get_data_op.output, # passing step_1.output as argument
+        arguments = get_data_op.output, #get_data_op.output, # passing step_1.output as argument
         file_outputs = {
-            'context': '/blob_path.txt' #name of the file with result
+            'blob-path': '/blob_path.txt' #name of the file with result
         }
    )
 
 #importing KFP compiler
-from kfp.compiler import Compiler#compiling the created pipeline
+#compiling the created pipeline
 Compiler().compile(train_recommender_model, 'train_modell_pipeline.zip')
 
