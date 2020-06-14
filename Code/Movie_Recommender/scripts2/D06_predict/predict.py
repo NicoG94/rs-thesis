e841fa1a2f4defa383b50112f6ec587fa75cb9cb
NicoG94 <grosskreuznico@gmail.com>
2020-06-14 20:41:15 +0200

    no message

--- Code/Movie_Recommender/scripts2/D06_predict/predict.py
+++ Code/Movie_Recommender/scripts2/D06_predict/predict.py
@@ -5,7 +5,7 @@
 import socket
 
 HOST = 'predict.dns.internal'  # Standard loopback interface address (localhost)
-PORT = 80        # Port to listen on (non-privileged ports are > 1023)
+PORT = 80         # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
