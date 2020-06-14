b49a793683d9c3414828e6891c98737c0a702fa3
NicoG94 <grosskreuznico@gmail.com>
2020-06-14 15:11:45 +0200

    no message

--- Code/Movie_Recommender/scripts2/D06_predict/predict.py
+++ Code/Movie_Recommender/scripts2/D06_predict/predict.py
@@ -1,3 +1,4 @@
+# Code/Movie_Recommender/scripts2/D06_predict/Dockerfile
 import pandas as pd
 import argparse
 from pathlib import Path
@@ -7,4 +8,4 @@ if __name__ == "__main__":
     print("Lets start V0.1.5")
 
     df = pd.read_csv("gcp_data/rs_predictions/prepared_data.csv")
-
+    print(df.head())
