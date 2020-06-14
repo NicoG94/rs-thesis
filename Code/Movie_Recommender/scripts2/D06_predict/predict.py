06f24e9b7a1b68aa955930d567c1076e8332a177
NicoG94 <grosskreuznico@gmail.com>
2020-06-14 19:29:19 +0200

    no message

--- Code/Movie_Recommender/scripts2/D06_predict/predict.py
+++ Code/Movie_Recommender/scripts2/D06_predict/predict.py
@@ -1,11 +1,25 @@
+#!/usr/bin/env python3
 # Code/Movie_Recommender/scripts2/D06_predict/Dockerfile
-import pandas as pd
-import argparse
-from pathlib import Path
 
+import socket
 
-if __name__ == "__main__":
-    print("Lets start V0.1.5")
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-    df = pd.read_csv("gcp_data/rs_predictions/prepared_data.csv")
-    print(df.head())
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            print("start")
+            data = conn.recv(1024)
+            if not data:
+                print("NO DATA")
+            #    break
+            print(f"DATA: {data}")
+            conn.sendall(data)
+            print("END")
+
+#df = pd.read_csv("gcp_data/rs_predictions/prepared_data.csv")
